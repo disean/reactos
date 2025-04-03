f69f002412031bd3239cc79be9360bc48e097898
Dmitry Borisov <di.sean@protonmail.com>
2025-04-04 00:25:06 +0600

    [VCRUNTIME] Fix string I/O inline asm
    Make string I/O inline asm code MSVC compatible.
    Avoid clobbering buffer operands when a CPU performs string I/O operations.
    CORE-20078

--- sdk/include/vcruntime/mingw32/intrin_x86.h
+++ sdk/include/vcruntime/mingw32/intrin_x86.h
@@ -1602,17 +1602,17 @@ __INTRIN_INLINE void __outdword(unsigned short Port, unsigned long Data)
 
 __INTRIN_INLINE void __outbytestring(unsigned short Port, unsigned char * Buffer, unsigned long Count)
 {
-	__asm__ __volatile__("rep; outsb" : : [Port] "d" (Port), [Buffer] "S" (Buffer), "c" (Count));
+	__asm__ __volatile__("rep; outsb" : "+S"(Buffer), "+c"(Count) : "d"(Port));
 }
 
 __INTRIN_INLINE void __outwordstring(unsigned short Port, unsigned short * Buffer, unsigned long Count)
 {
-	__asm__ __volatile__("rep; outsw" : : [Port] "d" (Port), [Buffer] "S" (Buffer), "c" (Count));
+	__asm__ __volatile__("rep; outsw" : "+S"(Buffer), "+c"(Count) : "d"(Port));
 }
 
 __INTRIN_INLINE void __outdwordstring(unsigned short Port, unsigned long * Buffer, unsigned long Count)
 {
-	__asm__ __volatile__("rep; outsl" : : [Port] "d" (Port), [Buffer] "S" (Buffer), "c" (Count));
+	__asm__ __volatile__("rep; outsl" : "+S"(Buffer), "+c"(Count) : "d"(Port));
 }
 
 __INTRIN_INLINE int __cdecl _inp(unsigned short Port)
